ebbf6c692ff5b2ba7c8803f568888bacf01b0855
Waygou <bruno.falcao@waygou.com>
2019-03-10 02:14:57 +0100

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
-Route::post('prechecks', PreChecksController::class)
-     ->name('prechecks');
-
-Route::post('ping', PingController::class)
-     ->name('ping');
+Route::post('prechecks', PreChecksController::class)->name('prechecks');
+Route::post('ping', PingController::class)->name('ping');
 
 /*
 Route::fallback(function () {
