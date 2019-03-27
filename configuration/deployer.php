663c2d5ac72b4f88a371e48d6e0cea7ac5b3fc65
Waygou <bruno.falcao@waygou.com>
2019-03-27 17:09:38 +0100

    Update deployer.php

--- configuration/deployer.php
+++ configuration/deployer.php
@@ -4,15 +4,20 @@ use Waygou\Deployer\Deployer;
 
 return [
 
+    /**
+     * Environment type:
+     * 'local' = Your local dev machine
+     * 'remote' = Your remote codebase server.
+     */
     'type' => env('DEPLOYER_TYPE', 'local'),
 
     /*
-     * Your base remote server URL for the deployer api calls.
-     * E.g.: http://www.johnsmith.com
-     * E.g.: https://www.clickandtry.com
+     * Your remove server information group.
      */
     'remote' => [
+        // Your remote server URL. Configured in your .env when installing deployer on your local server.
         'url' => env('DEPLOYER_REMOTE_URL'),
+
         // Your route prefix, default is <your-server-url>/deployer.
         'prefix' => '/deployer',
     ],
@@ -20,35 +25,45 @@ return [
     /*
      * What scripts/processes do you want to run before and after your
      * deployment was completed?
+     * You can use (as many as you want):
+     * Artisan commands. E.g.: 'cache:clear'
+     * Invokable Classes: E.g.: MyClass::class (will call your __invoke() directly).
+     * Custom Class methods: E.g.: 'MyClass@myMethod'.
      */
     'scripts' => [
         'before_deployment' => [
-            'route:list', // This is considered an Artisan command.
-            'view:clear',
-            /*
-             * You can also use the following options:
-             * YourInvokableClass::class - Will run the __invoke() magic method.
-             * 'YourClass @ method' - Will run your class and the respective method on it.
-             */
+            'route:list', // Just an example.
         ],
         'after_deployment' => [
-            'route:list',
+            'cache:clear',
+            'view:clear',
+            'clear'
         ],
     ],
 
     // What's the codebase you want to upload to your remote server?
     'codebase' => [
-        'folders' => ['App', 'packages/waygou/deployer'],
-        'files' => ['database/factories/UserFactory.php', 'resources/js/app.js'],
+        'folders' => [
+            'app'
+        ],
+        'files' => [
+            'database/factories/UserFactory.php',
+            'resources/js/app.js'
+        ],
+        // Files or folders where you don't want to override them on your remote server.
+        'whitelist' => [
+            '.env'
+        ]
     ],
 
-    // Storage to put the zip files that are created.
+    // Storage where it should be created all the transaction folders for your codebase.
     'storage' => [
         'path' => storage_path('app/deployer'),
     ],
 
     /*
      * OAuth information + remote<->local token information.
+     * Automatically filled on the remote server installation.
      */
     'oauth' => [
         'client' => env('DEPLOYER_OAUTH_CLIENT'),
@@ -57,6 +72,7 @@ return [
 
     /*
      * Local / Remote token. Must be the same in both environments.
+     * Automatically created on your local or remote server installations.
      */
     'token' => env('DEPLOYER_TOKEN'),
 ];
