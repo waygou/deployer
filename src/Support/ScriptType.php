e7786af0d8cf2a6e6b55ac7c882753f39e5ded5b
Waygou <bruno.falcao@waygou.com>
2019-04-05 23:51:01 +0200

    Create ScriptType.php

---
+++ src/Support/ScriptType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Waygou\Deployer\Support;
+
+final class ScriptType
+{
+    const ARTISAN = 'artisan';
+    const CLASSMETHOD = 'class_method';
+    const SHELLCMD = 'shell_cmd';
+}
